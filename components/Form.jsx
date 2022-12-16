d4509a7fddc91a5e17c28daa4268a6591d034790
ummagohilpa <umma.gohil@paconsulting.com>
2022-12-16 11:24:20 +0000

    tidy up

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,19 +1,22 @@
-const Form = () => (
-  <div>
-    <label>Name:</label>
-    <input
-      onChange={() =>
-        console.log(
-          "input change should be stored in state and sent to results component"
-        )
-      }
-    />
-    <button
-      onClick={() => console.log("submit the input and route to results")}
-    >
-      submit
-    </button>
-  </div>
-);
+const Form = () => {
+  const onChange = () => {
+    console.log(
+      "input change to be stored in local state and sent to results component"
+    );
+  };
+
+  const onClick = () => {
+    console.log(
+      "take the onChange value stored in local state and route to results"
+    );
+  };
+  return (
+    <div>
+      <label>Name:</label>
+      <input onChange={onChange} />
+      <button onClick={onclick}>submit</button>
+    </div>
+  );
+};
 
 export default Form;
